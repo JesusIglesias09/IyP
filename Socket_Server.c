144f7d6990fcd864da47ace12ce28623223410e6
Jesus Iglesias <alekks09@pop-os.localdomain>
2021-11-22 22:25:37 -0600

    Fully working server and client with image transfer

--- Socket_Server.c
+++ Socket_Server.c
@@ -8,12 +8,10 @@
 #include <stdint.h>
 #define PORT 8000
 
-
 int main(int argc, char  *argv[])
 {
-    int32_t server_fd; 
-    int32_t new_socket; 
-    int32_t valread;
+    int32_t server_fd;
+    int32_t new_socket;
     struct sockaddr_in address;
     int32_t opt = 1;
     int32_t addrlen = sizeof(address);
@@ -25,7 +23,7 @@ int main(int argc, char  *argv[])
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-       
+
     // Forcefully attaching socket to the port 8080
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                   &opt, sizeof(opt)))
@@ -36,10 +34,10 @@ int main(int argc, char  *argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
-       
-    
+
+
     printf("PHASE 1\n");
-    if (bind(server_fd, (struct sockaddr *)&address, 
+    if (bind(server_fd, (struct sockaddr *)&address,
                                  sizeof(address))<0)
     {
         perror("bind failed");
@@ -52,19 +50,29 @@ int main(int argc, char  *argv[])
         return -1;
     }
     printf("PHASE 3\n");
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                        (socklen_t*)&addrlen))<0)
     {
         perror("accept");
         return 1;
     }
-    
+
     printf("ENDING PHASE\n");
-    valread = read( new_socket , buffer, 1024);
-    
-    printf("BUFFER DATA %i\n",buffer[2]);
-    
-    for (int i = 0; ( buffer[2] + 6) >= i; i++) printf("%x\n",buffer[i] );
-    
+    //Read pic size
+    int size;
+    read( new_socket , &size, sizeof(int));
+    printf("Size image =  %d\n",size);
+
+    //Read pic byte array
+    printf("Reading Picture Byte Array\n");
+    char p_array[100];
+    FILE *image = fopen("image.jpg", "w");
+    int nb = read(new_socket, p_array, 100);
+    while (nb > 0) {
+        fwrite(p_array, 1, nb, image);
+        nb = read(new_socket, p_array, 100);
+    }
+    fclose(image);
+    printf("Image created");
     return 0;
 }
